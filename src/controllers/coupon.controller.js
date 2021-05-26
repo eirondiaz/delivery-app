3a808d9bc1c7df1d7d5ff4789a11deff86a17f94
Lewis Matos <lewiismb1@example.com>
2021-05-26 17:04:15 +0200

    ...

--- src/controllers/coupon.controller.js
+++ src/controllers/coupon.controller.js
@@ -27,7 +27,7 @@ exports.getAll = async (req, res) => {
 exports.getByCode = async (req, res) => {
     const { code } = req.params
     try {
-        const coupon = await Coupon.findOne({code})
+        const coupon = await (await Coupon.findOne({code})).populate('usedBy')
 
         if (!coupon)
             return res.status(404).json({ok: false, msg: 'coupon not found'})
